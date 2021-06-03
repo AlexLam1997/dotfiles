e306685f8f7965ad65cdfccd02be3d35e7d2df55
Alexander Lam <alexander.lam@mail.mcgill.ca>
2021-06-03 14:26:23 -0400

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+# Only run nested steps in Spin workspaces.
+if [[ "$SPIN" = 1 ]]
+then
+  # Only run nested steps in Spin + shopify/shopify workspaces.
+  if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
+  then
+    cartridge insert AlexLam1997/bootstrap
+    . /cartridges/bootstrap/setup.sh
+  fi
+fi
