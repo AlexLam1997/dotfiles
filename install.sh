6185bdbf83fcb3443b9862c63e974d8b6402a939
Alexander Lam <alexander.lam@mail.mcgill.ca>
2021-09-24 18:16:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ DOTFILE_DIR=$(pwd)
 # Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Get ohmyzsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 for FILE in $FILES; do
   ln -sf $DOTFILE_DIR/$FILE ~/$FILE
 done
