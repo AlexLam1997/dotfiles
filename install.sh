37a1045e9b7e2fba21df5611e11d5f7625e65d6b
Alexander Lam <alexander.lam@mail.mcgill.ca>
2021-09-24 18:07:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ DOTFILE_DIR=$(pwd)
 
 #git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 for FILE in $FILES; do
   ln -sf $DOTFILE_DIR/$FILE ~/$FILE
 done
