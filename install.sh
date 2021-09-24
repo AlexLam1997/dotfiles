9559ee4c2fac0aaf4d9d8f4619e3e29a4a9da31f
Alexander Lam <alexander.lam@mail.mcgill.ca>
2021-09-24 17:54:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 FILES=".gitconfig .tmux.conf .gitignore .zpreztorc .zshrc"
 DOTFILE_DIR=$(pwd)
 
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+#git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 for FILE in $FILES; do
   ln -sf $DOTFILE_DIR/$FILE ~/$FILE
