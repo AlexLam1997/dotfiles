24cc0c28773b099c1b3fbb03e387aabbb00b5d96
Alexander Lam <alexander.lam@shopify.com>
2021-09-28 17:32:18 +0000

    Modifies

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
+#!/bin/zsh
 FILES=".gitconfig .tmux.conf .gitignore .zshrc"
 DOTFILE_DIR=$(pwd)
 
 #git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 # Install ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if ! command -v zsh &> /dev/null
+then
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  # Get ohmyzsh plugins
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
-# Get ohmyzsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 for FILE in $FILES; do
   ln -sf $DOTFILE_DIR/$FILE ~/$FILE
