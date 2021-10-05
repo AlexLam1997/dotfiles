3776e73a05edefef4865a41643c2b8301c84a0f3
Alexander Lam <alexander.lam@mail.mcgill.ca>
2021-10-05 11:20:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,14 @@
 FILES=".gitconfig .tmux.conf .gitignore .zshrc"
 DOTFILE_DIR=$(pwd)
 
-#git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
 # Install ohmyzsh
-if ! command -v $ZSH &> /dev/null
-then
-  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &> log.out || echo "oh-my-zsh already installed."
-  # Get ohmyzsh plugins
-  {
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &> log.out
-  } || {
-    echo "auto-suggestions already installed" 
-  }
-fi
-
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &> log.out || echo "oh-my-zsh already installed."
+# Get ohmyzsh plugins
+{
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &> log.out
+} || {
+  echo "auto-suggestions already installed" 
+}
 
 for FILE in $FILES; do
   ln -sf $DOTFILE_DIR/$FILE ~/$FILE
