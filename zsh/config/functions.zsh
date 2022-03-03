3a25e527ed85b27ff5e5b94617e64f5adb40f50d
Alexander Lam <alexander.lam@shopify.com>
2022-03-03 16:22:19 -0500

    Remove legacy commands and adds documentation

--- zsh/config/functions.zsh
+++ zsh/config/functions.zsh
@@ -1,4 +1,4 @@
-# Functions
+# Github functions
 function ff(){
   git add .
   git commit -am'f' --fixup HEAD
@@ -10,16 +10,25 @@ function ff(){
   fi
 }
 
-# Isospin functions
+# Isospin functions - run within a spin instance
+
+# Restarts the spin 'server' proc in the foreground to allow the debugger to be attached
 function debug() {
   local repo=${1:-email}    
   sc stop proc-shopify--"$repo"@server.service
   RAILS_ENV=development RACK_TIMEOUT_SERVICE_TIMEOUT=false bin/rails server -p $PORT
-  # Add commands to start job processes in other tabs
 }
 
+# Kills active servers
+function kill(){
+  kill -9 $(cat tmp/pids/server.pid)
+}
+
+# Utility functions - run locally
+
+# Opens sql ace with db config for current instance.
+# Accepts repo name as first argument, defaults to email
 function open_db {
-  # Opens sql ace with db config for current instance. $1 is the repo name
   local repo=${1:-email};
   if [[ "$repo" = email ]]
   then
@@ -28,66 +37,33 @@ function open_db {
     local schema=${2:-shopify_dev_shard_0}
   fi
 
-  open "mysql://root@$repo.$(spin show -o fqdn --latest):$(spin shell -- cat /home/spin/src/github.com/Shopify/$repo/.shadowenv.d/500-spin-svc-mysql.lisp | grep MYSQL_PORT | egrep -o '\d*')/$schema" -a "Sequel Ace"
+  spin_instance_name=$(spin show -o name)
+  spin_fqdn=$(spin show $spin_instance_name -o fqdn)
+  my_sql_port=$(spin shell $spin_instance_name -- cat /home/spin/src/github.com/Shopify/$repo/.shadowenv.d/500-spin-svc-mysql.lisp | grep MYSQL_PORT | egrep -o '\d*')
+  open "mysql://root@$repo.$spin_fqdn:$my_sql_port/$schema" -a "Sequel Ace"
 }
 
-#Spin functions
+# The following functions open the specified resource
 function graphql() {
-  if [[ $1 = 'legacy' ]]
-  then
-    open "https://app.$(spin show -o fqdn --latest)/services/internal/shops/1/graphql"
-  else
-    open "https://app.shopify.$(spin show -o fqdn --latest)/services/internal/shops/1/graphql"
-  fi
-}
-
-# Kills active servers
-function kill(){
-  kill -9 $(cat tmp/pids/server.pid)
+  open "https://app.shopify.$(spin show -o fqdn --latest)/services/internal/shops/1/graphql"
 }
 
 function graphql_email() {
-  # https://email.nui9.alexander-lam.us.spin.dev/graphiql
-  if [[ $1 = 'legacy' ]]
-  then
-    open "https://$(spin show -o fqdn --latest)/graphiql"
-  else
-    open "https://email.$(spin show -o fqdn --latest)/graphiql"
-  fi
+  open "https://email.$(spin show -o fqdn --latest)/graphiql"
 }
 
 function admin() {
-  if [[ $1 = 'legacy' ]]
-  then
-    open "https://$(spin show -o fqdn --latest)/admin"
-  else
-    open "https://shop1.shopify.$(spin show -o fqdn --latest)/admin"
-  fi
+  open "https://shop1.shopify.$(spin show -o fqdn --latest)/admin"
 }
 
 function internal_email() {
-  if [[ $1 = 'legacy' ]]
-  then
-    open "https://$(spin show -o fqdn --latest)/management/internal/shops/1"
-  else
-    open "https://email.$(spin show -o fqdn --latest)/management/internal/shops/1"
-  fi
+  open "https://email.$(spin show -o fqdn --latest)/management/internal/shops/1"
 }
 
 function internal_shopify(){
-  if [[ $1 = 'legacy' ]]
-  then
-    open "https://app.$(spin show -o fqdn --latest)/services/internal/shops/1"
-  else
-    open "https://app.shopify.$(spin show -o fqdn --latest)/services/internal/shops/1"
-  fi
+  open "https://app.shopify.$(spin show -o fqdn --latest)/services/internal/shops/1"
 }
 
 function letter_opener() {
-  if [[ $1 = 'legacy' ]]
-  then
-    open "https://$(spin show -o fqdn --latest)/letter_opener"
-  else
-    open "https://email.$(spin show -o fqdn --latest)/letter_opener"
-  fi
+  open "https://email.$(spin show -o fqdn --latest)/letter_opener"
 }
